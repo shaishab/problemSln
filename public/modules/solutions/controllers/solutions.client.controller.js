bb6d1b02bf13b7171eab862bcb9dc9997eb1b747
Shaishab <shaishab.cse@gmail.com>
2015-08-07 19:43:20 +0600

    add another dropdown list

--- public/modules/solutions/controllers/solutions.client.controller.js
+++ public/modules/solutions/controllers/solutions.client.controller.js
@@ -6,6 +6,7 @@ angular.module('solutions').controller('SolutionsController', ['$scope', '$state
 		$scope.authentication = Authentication;
 
 		$scope.dropdownList = function() {
+			$scope.selectedDist={};
 			$scope.users =[
 				{id: 11, name: 'shaishab'},
 				{id: 12, name: 'roy'},
@@ -14,8 +15,33 @@ angular.module('solutions').controller('SolutionsController', ['$scope', '$state
 			];
 			$scope.usr ={};
 			$scope.selectedUser = $scope.users[1];
+
+			$scope.districts = [
+				{id: 1, name: 'Dhaka'},
+				{id: 2, name: 'Goplaganj'},
+				{id: 3, name: 'Faridpur'}
+			];
+
+			$scope.thanas = [
+				{id: 1, name: 'Mirpur', dId: 1},
+				{id: 2, name: 'Uttra', dId: 1},
+				{id: 3, name: 'Shahabag', dId: 1},
+				{id: 4, name: 'Kotalipara', dId: 2},
+				{id: 5, name: 'Kashiani', dId: 2},
+				{id: 6, name: 'Moksedpur', dId: 2},
+				{id: 7, name: 'Vanga', dId: 3},
+				{id: 8, name: 'faridpur', dId: 3}
+			];
+			$scope.filterExpression = function(thana) {
+				return (thana.dId === $scope.selectedDist.id );
+			};
+			$scope.vm ={};
+			$scope.newQuestion ={};
+			$scope.vm.questionsPositions = [12,14];
+			$scope.newQuestion.positionOnSurvey = $scope.vm.questionsPositions[0];
 		};
-
-
 	}
 ]);
+
+
+
